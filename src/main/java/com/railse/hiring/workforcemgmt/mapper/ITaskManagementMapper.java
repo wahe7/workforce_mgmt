76b76cffa85f0e35c37c6650fee61798d9e421ec
wahe7 <waheguru002200@gmail.com>
2025-08-02 17:53:59 +0530

    itaskmanagement mapper

---
+++ src/main/java/com/railse/hiring/workforcemgmt/mapper/ITaskManagementMapper.java
@@ -0,0 +1,19 @@
+package com.railse.hiring.workforcemgmt.mapper;
+
+import com.railse.hiring.workforcemgmt.dto.TaskManagementDto;
+import com.railse.hiring.workforcemgmt.model.TaskManagement;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ITaskManagementMapper {
+
+  TaskManagementDto modelToDto(TaskManagement model);
+
+  TaskManagement dtoToModel(TaskManagementDto dto);
+
+  List<TaskManagementDto> modelListToDtoList(List<TaskManagement> models);
+}
