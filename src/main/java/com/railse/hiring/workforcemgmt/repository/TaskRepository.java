c14b9ac884cf61fb659db18197e6c428a8a85232
wahe7 <waheguru002200@gmail.com>
2025-08-02 17:54:22 +0530

    task repository

---
+++ src/main/java/com/railse/hiring/workforcemgmt/repository/TaskRepository.java
@@ -0,0 +1,19 @@
+package com.railse.hiring.workforcemgmt.repository;
+
+import com.railse.hiring.workforcemgmt.model.TaskManagement;
+import com.railse.hiring.workforcemgmt.common.model.enums.ReferenceType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+  Optional<TaskManagement> findById(Long id);
+
+  TaskManagement save(TaskManagement task);
+
+  List<TaskManagement> findAll();
+
+  List<TaskManagement> findByReferenceIdAndReferenceType(Long referenceId, ReferenceType referenceType);
+
+  List<TaskManagement> findByAssigneeIdIn(List<Long> assigneeIds);
+}
